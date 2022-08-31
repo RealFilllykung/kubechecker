4ab007e745604b82c0a152a0e95c39a60f3a728a
Tharathep Klinla-or <filllykunggm@gmail.com>
2022-08-31 10:54:20 +0700

    Add the feature of loop check and choose whether to get the logs or describe

--- kubechecker.py
+++ kubechecker.py
@@ -1,9 +1,46 @@
 import os
 import subprocess
+import platform
+import time
 
-os.system('clear')
+def clearScreen():
+	mySystem = platform.system()
+	if mySystem == "Windows":
+		os.system('cls')
+	else:
+		os.system('clear')
+
+clearScreen()
+
+print("Loop checking mode: ")
+print("1. Loop check")
+print("2. Other mode")
+loopCheckMode = input("Chosen option: ")
+SLEEP_TIME = 5
+
+if loopCheckMode == "1":
+	while True:
+		print("Refreshing kube list")
+		result = subprocess.run(["kubectl","get","pods","-n","xom-logistics-dev"],stdout=subprocess.PIPE)
+		lines = result.stdout.splitlines()
+		index = 0
+		options = []
+		for byteLine in lines:
+			if index == 0:
+				index = index + 1
+				continue
+			line = byteLine.decode("utf-8")
+			lineSplit = line.split(" ")
+			options.append(lineSplit[0])
+		index = 1
+		for option in options:
+			print(str(index) + ") "  + option)
+			index = index + 1
+		time.sleep(SLEEP_TIME)
+		clearScreen()
+
+
 print("Gathering pods list...")
-
 result = subprocess.run(["kubectl","get","pods","-n","xom-logistics-dev"],stdout=subprocess.PIPE)
 lines = result.stdout.splitlines()
 index = 0
@@ -22,8 +59,18 @@ for option in options:
 	print(str(index) + ") "  + option)
 	index = index + 1
 
-answer = input("Choosen option: ")
+answer = input("Chosen option: ")
 chosenAnswer = int(answer) - 1
 
-osArgs = "kubectl logs -f " + options[chosenAnswer] + " -n xom-logistics-dev"
-os.system(osArgs)
+print("Available mode: ")
+print("1. Logs")
+print("2. Describe")
+mode = input("Chosen mode: ")
+chosenMode = int(answer)
+
+if chosenMode == 1:
+	osArgs = "kubectl logs -f " + options[chosenAnswer] + " -n xom-logistics-dev"
+	os.system(osArgs)
+elif chosenMode == 2:
+	osArgs = "kubectl describe pod " + options[chosenAnswer] + " -n xom-logistics-dev"
+	os.system(osArgs)
